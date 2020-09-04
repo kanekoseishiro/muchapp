8b2c0de93f496daba6cc93b872d1e2f41d641fef
kanekoseishiro <sei1025kaneko@gmail.com>
2020-09-04 12:51:54 +0900

    遷移さきの一覧ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "users#index"
-  # resourses: users, only:[:index]
+  resources :members, only: :index
+  # resources :users, only:[:index]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_030810) do
+ActiveRecord::Schema.define(version: 2020_09_04_032628) do
+
+  create_table "members", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
