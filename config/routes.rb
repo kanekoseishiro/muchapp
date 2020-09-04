78c92a6f3e8a7cb01e53a822b6e1c63a3046d438
kanekoseishiro <sei1025kaneko@gmail.com>
2020-09-05 01:22:46 +0900

    memberコントローラー、ビュー、モデルの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "members#index"
-  resources :members, only: :index
-  resources :users, only: :index
+  root "users#index"
+  get 'users', to: 'users#member'
+  # resources :users, only: :index
 end
