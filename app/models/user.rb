1d369b557221d066a1970cf91eb6d0c40b7f7134
kanekoseishiro <sei1025kaneko@gmail.com>
2020-09-04 19:39:16 +0900

    テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname, length: { maximum: 20 }
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }, length: { minimum: 6 }
-    validates :prefecture, numericality: { other_than: 1 }
-    validates :phone_number, length: { maximum: 11 }
     validates :age, format: { with:/[0-9]\d/ }
     validates :gender, numericality: { other_than: 1 }
     validates :birthday
+    validates :prefecture, numericality: { other_than: 1 }
+    validates :phone_number, length: { maximum: 11 }
   end
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
