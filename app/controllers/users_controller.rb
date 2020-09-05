dd03cdfc9e47d08b69d077f48fb7e3d2fe385def
kanekoseishiro <sei1025kaneko@gmail.com>
2020-09-05 11:34:59 +0900

    ログアウト時のページ遷移先の設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
+  before_action :move_to_users, only: [:index]
+
   def index
   end
 
   def member
+  end
+
+  private
+
+  def move_to_users
+    unless user_signed_in?
+      redirect_to users_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   devise_for :users
   root "users#index"
   get 'users', to: 'users#member'
