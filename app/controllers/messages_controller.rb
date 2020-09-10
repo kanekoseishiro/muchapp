db26a76e487e9c332e253f1685f7715211fbd42e
kanekoseishiro <sei1025kaneko@gmail.com>
2020-09-10 10:33:48 +0900

    チャットルーム機能追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,23 @@
 class MessagesController < ApplicationController
+  def index
+    @message = Message.new
+    @messages = Message.includes(:user)
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    if @message.save
+      redirect_to messages_path
+    else
+      @messages = Message.includes(:user)
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+  end
 end
